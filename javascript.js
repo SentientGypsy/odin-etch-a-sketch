f04038e3a144515c1800a6314027e3ea4641313b
sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-11-06 09:27:45 -0600

    add painting to the divs

--- javascript.js
+++ javascript.js
@@ -2,16 +2,45 @@ const canvas = document.createElement("div");
 const container = document.querySelector(".container");
 
 
-for (var i = 0; i <= 15; i++){
-    const canvas = document.createElement("div");
-    canvas.classList.add("canvas-div");
-    canvas.textContent = "%";
-    container.appendChild(canvas);
-    console.log("appended");
-}
 
-const canvas_div = document.querySelector(".canvas-div");
 
-canvas_div.addEventListener("mouseenter", () => {
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    for (var i = 0; i <= 15; i++){
+        const canvas = document.createElement("div");
+        canvas.classList.add("canvas-div");
+        canvas.textContent = "%";
+        container.appendChild(canvas);
+        console.log("appended");
+    }
+
+
+    document.querySelectorAll(".canvas-div").forEach( el => {
+        el.addEventListener("mouseenter", () => {
+            el.setAttribute("style", "background: black");
+            
+            
+            
+        });
+    });
     
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
